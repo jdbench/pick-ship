c753eb7b80b82225e3e0b31118646c06b08e29ad
jdbench <justin.d.bench@gmail.com>
2022-07-09 02:31:51 -0600

    fixes

--- web/frontend/components/index.js
+++ web/frontend/components/index.js
@@ -1,3 +1,4 @@
 export { ProductsCard } from "./ProductsCard";
 export { NavbarComponent } from "./Navbar";
+export { ResourceListWithProducts } from "./resourceList.js";
 export * from "./providers";
--- web/frontend/pages/products.jsx
+++ web/frontend/pages/products.jsx
@@ -3,10 +3,12 @@ import{
 } 
 from "@shopify/polaris";
 
+import { resourceList } from "../components"
 export default function products() {
   return (
     <Page>
         <div>products</div>
+        <resourceList />
     </Page>
   )
 }
